a55f94cfbd2f9c02b6584bee5549e271f956b56a
Tresor Kadima <kadt2022@gmail.com>
2026-07-26 12:56:29 -0400

    fix(security): restrict debug controller to test profile

--- takibo-security-management/src/main/java/com/takibo/securitymanagement/apitest/SecurityAccessTestController.java
+++ takibo-security-management/src/main/java/com/takibo/securitymanagement/apitest/SecurityAccessTestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-//@Profile({"dev", "test"})
+@Profile("test")
 @RestController
 @RequestMapping("/debug/secure")
 @RequiredArgsConstructor
